1f4e719acaea9f5907963ed7736f9b63f64f3bce
yahayagodiwa <yahayagodiwa8@gmail.com>
2025-12-02 10:04:12 +0100

    user in login

--- controllers/authController.js
+++ controllers/authController.js
@@ -114,7 +114,7 @@ const login = async (req, res) => {
       }
     );
 
-    return res.status(200).json({ token });
+    return res.status(200).json({ token, existingUser });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: "An error occurred during login" });
